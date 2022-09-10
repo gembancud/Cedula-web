c4943e128ef2a92a0e5f392999b598ab6cc5d16c
Gil Emmanuel Bancud <gembancud@gmail.com>
2022-09-10 15:31:39 +0800

    fix: mantine next ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,21 @@ import "../styles/global.css";
 import { MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
 
+import initAuth from "../utils/initAuth";
+import { rtlCache } from "../utils/rtl-cache";
+
+initAuth();
+
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <MantineProvider theme={{}}>
+  <MantineProvider
+    withGlobalStyles
+    withNormalizeCSS
+    emotionCache={rtlCache}
+    theme={{
+      dir: "rtl",
+      colorScheme: "light",
+    }}
+  >
     <Component {...pageProps} />
   </MantineProvider>
 );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,29 @@
-import { createGetInitialProps } from "@mantine/next";
+import { createStylesServer, ServerStyles } from "@mantine/next";
+import type { DocumentContext } from "next/document";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
 import { AppConfig } from "@/utils/AppConfig";
 
-const getInitialProps = createGetInitialProps();
+import { rtlCache } from "../utils/rtl-cache";
+
+const stylesServer = createStylesServer(rtlCache);
 // Need to create a custom _document because i18n support is not compatible with `next export`.
 class MyDocument extends Document {
-  static getInitialProps = getInitialProps;
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <ServerStyles
+          html={initialProps.html}
+          server={stylesServer}
+          key="styles"
+        />,
+      ],
+    };
+  }
 
   // eslint-disable-next-line class-methods-use-this
   render() {
