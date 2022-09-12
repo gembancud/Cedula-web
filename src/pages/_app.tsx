93e6a1b310c054ac821cc01983b16340ebc40bb2
Gil Emmanuel Bancud <gembancud@gmail.com>
2022-09-12 18:20:40 +0800

    feat: add themes, footer, and header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,38 @@
 import "../styles/global.css";
 
-import { MantineProvider } from "@mantine/core";
+import type { ColorScheme } from "@mantine/core";
+import { ColorSchemeProvider, MantineProvider } from "@mantine/core";
 import type { AppProps } from "next/app";
+import { useState } from "react";
 
 import initAuth from "../utils/initAuth";
 import { rtlCache } from "../utils/rtl-cache";
 
 initAuth();
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <MantineProvider
-    withGlobalStyles
-    withNormalizeCSS
-    emotionCache={rtlCache}
-    theme={{
-      dir: "rtl",
-      colorScheme: "light",
-    }}
-  >
-    <Component {...pageProps} />
-  </MantineProvider>
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        emotionCache={rtlCache}
+        theme={{
+          dir: "rtl",
+          colorScheme,
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+};
 
 export default MyApp;
