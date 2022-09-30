3dedb3f719069a0a708053bfb74c7baa79fcf9a2
Gil Emmanuel Bancud <gembancud@gmail.com>
2022-09-30 20:24:49 +0800

    fix: sanity client email

--- src/utils/initAuth.ts
+++ src/utils/initAuth.ts
@@ -18,7 +18,9 @@ const initAuth = () => {
     firebaseAdminInitConfig: {
       credential: {
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+
+        // @ts-ignore
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 
         // The private key must not be accessible on the client side.
         // privateKey: process.env.FIREBASE_PRIVATE_KEY
