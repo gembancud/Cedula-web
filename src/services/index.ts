a77d00d7b3dff54940979f26c7bb42ff0df2faab
Gil Emmanuel Bancud <gembancud@gmail.com>
2022-10-26 01:07:02 +0800

    feat: add services api for verify

---
+++ src/services/index.ts
@@ -0,0 +1,3 @@
+export const url = process.env.NEXT_PUBLIC_BACKEND_API_URL!;
+
+export { VerifyUser } from "./verify";
---
+++ src/services/verify.ts
@@ -0,0 +1,38 @@
+const url = process.env.NEXT_PUBLIC_BACKEND_API_URL!;
+
+interface VerifyUserInterface {
+  token: string;
+  email: string;
+  org: string;
+  evaluation: string;
+  comment: string;
+}
+
+export const VerifyUser = async ({
+  token,
+  email,
+  org,
+  evaluation,
+  comment,
+}: VerifyUserInterface): Promise<any> => {
+  try {
+    const verifyResponse = await fetch(`${url}/verify`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token || "unauthenticated",
+      },
+      body: JSON.stringify({
+        email,
+        org,
+        evaluation,
+        comment,
+      }),
+    });
+
+    return await verifyResponse.json();
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+};
